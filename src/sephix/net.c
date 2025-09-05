45b3e08c74eb2e7d6babbbcee9590e2ab74eb168
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-09-05 17:09:16 +0700

    fix net functions

--- src/sephix/net.c
+++ src/sephix/net.c
@@ -2,6 +2,7 @@
 #include "sephix/sandbox.h"
 #include "util.h"
 
+#include <linux/if_link.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 #include <net/if.h>
@@ -17,48 +18,19 @@
 #include <netlink/socket.h>
 
 int
-net__set_link_updown(const char *ifname, int up)
+set_link_updown(struct nl_sock *sock, struct rtnl_link *link, int up)
 {
 	int exit_code = 0;
-	struct nl_sock *sock;
-	struct nl_cache *link_cache;
-	struct rtnl_link *link;
-	struct rtnl_link *link_change;
 	int err;
-
-	sock = nl_socket_alloc();
-	if (sock == NULL) {
-		PERROR("nl_socket_alloc");
-		_EXIT(out, -1);
-	}
-
-	if ((err = nl_connect(sock, NETLINK_ROUTE)) < 0) {
-		LOG_ERROR("nl_connect: %s", nl_geterror(err));
-		_EXIT(out_sock, -1);
-	}
-
-	if ((err = rtnl_link_alloc_cache(sock, AF_UNSPEC, &link_cache)) < 0) {
-		LOG_ERROR("rtnl_link_alloc_cache: %s", nl_geterror(err));
-		_EXIT(out_sock, -1);
-	}
-
-	link = rtnl_link_get_by_name(link_cache, ifname);
-	if (link == NULL) {
-		fprintf(stderr, "sephix: no such interface '%s'\n", ifname);
-		_EXIT(out_link_cache, -1);
-	}
+	struct rtnl_link *link_change;
 
 	link_change = rtnl_link_alloc();
 	if (link_change == NULL) {
 		PERROR("rtnl_link_change");
-		_EXIT(out_link, -1);
+		_EXIT(out, -1);
 	}
 
-	if (up)
-		rtnl_link_set_flags(link_change, IFF_UP);
-	else
-		rtnl_link_unset_flags(link_change, IFF_UP);
-
+	(up ? rtnl_link_set_flags : rtnl_link_unset_flags)(link_change, IFF_UP);
 	if ((err = rtnl_link_change(sock, link, link_change, 0)) < 0) {
 		LOG_ERROR("rtnl_link_change: %s", nl_geterror(err));
 		_EXIT(out_link_change, -1);
@@ -66,22 +38,17 @@ net__set_link_updown(const char *ifname, int up)
 
 out_link_change:
 	rtnl_link_put(link_change);
-out_link:
-	rtnl_link_put(link);
-out_link_cache:
-	nl_cache_free(link_cache);
-out_sock:
-	nl_socket_free(sock);
 out:
 	return exit_code;
 }
 
 int
-add_addr(struct nl_sock *sock, int ifindex, const char *cidr)
+add_addr(struct nl_sock *sock, struct rtnl_link *link, const char *cidr)
 {
 	int exit_code = 0;
 	struct rtnl_addr *addr;
 	struct nl_addr *local;
+	int ifindex;
 	int err;
 
 	addr = rtnl_addr_alloc();
@@ -93,6 +60,12 @@ add_addr(struct nl_sock *sock, int ifindex, const char *cidr)
 	if ((err = nl_addr_parse(cidr, AF_INET, &local)) < 0) {
 		LOG_ERROR("nl_addr_parse(%s): %s", cidr, nl_geterror(err));
 		_EXIT(out_addr, -1);
+	}
+
+	ifindex = rtnl_link_get_ifindex(link);
+	if (ifindex <= 0) {
+		LOG_ERROR("rtnl_link_get_ifindex");
+		_EXIT(out_local, -1);
 	}
 
 	rtnl_addr_set_local(addr, local);
@@ -165,22 +138,11 @@ create_veth(const char *if1, const char *ip1, const char *if2, const char *ip2)
 		_EXIT(out_l1, -1);
 	}
 
-	int ifindex1 = rtnl_link_get_ifindex(l1);
-	if (ifindex1 <= 0) {
-		LOG_ERROR("rtnl_link_get_ifindex");
-		_EXIT(out_l2, -1);
-	}
-	int ifindex2 = rtnl_link_get_ifindex(l2);
-	if (ifindex2 <= 0) {
-		LOG_ERROR("rtnl_link_get_ifindex");
-		_EXIT(out_l2, -1);
-	}
-
-	if (add_addr(sock, ifindex1, ip1) < 0) {
+	if (ip1 && add_addr(sock, l1, ip1) < 0) {
 		LOG_ERROR("add_addr");
 		_EXIT(out_l2, -1);
 	}
-	if (add_addr(sock, ifindex2, ip2) < 0) {
+	if (ip2 && add_addr(sock, l2, ip2) < 0) {
 		LOG_ERROR("add_addr");
 		_EXIT(out_l2, -1);
 	}
@@ -244,6 +206,51 @@ move_if_to_ns(const char *ifname, pid_t target_pid)
 
 out_link_change:
 	rtnl_link_put(link_change);
+out_link:
+	rtnl_link_put(link);
+out_link_cache:
+	nl_cache_free(link_cache);
+out_sock:
+	nl_socket_free(sock);
+out:
+	return exit_code;
+}
+
+int
+net__set_link_updown(const char *ifname, int up)
+{
+	int exit_code = 0;
+	struct nl_sock *sock;
+	struct nl_cache *link_cache;
+	struct rtnl_link *link;
+	int err;
+
+	sock = nl_socket_alloc();
+	if (sock == NULL) {
+		PERROR("nl_socket_alloc");
+		_EXIT(out, -1);
+	}
+
+	if ((err = nl_connect(sock, NETLINK_ROUTE)) < 0) {
+		LOG_ERROR("nl_connect: %s", nl_geterror(err));
+		_EXIT(out_sock, -1);
+	}
+
+	if ((err = rtnl_link_alloc_cache(sock, AF_UNSPEC, &link_cache)) < 0) {
+		LOG_ERROR("rtnl_link_alloc_cache: %s", nl_geterror(err));
+		_EXIT(out_sock, -1);
+	}
+
+	link = rtnl_link_get_by_name(link_cache, ifname);
+	if (link == NULL) {
+		fprintf(stderr, "sephix: no such interface '%s'\n", ifname);
+		_EXIT(out_link_cache, -1);
+	}
+
+	if (set_link_updown(sock, link, up) < 0) {
+		_EXIT(out_link, -1);
+	}
+
 out_link:
 	rtnl_link_put(link);
 out_link_cache:
